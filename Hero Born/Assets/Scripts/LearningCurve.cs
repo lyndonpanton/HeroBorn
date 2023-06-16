5f0c1d5bb765a92d157f867a3739b0db1c6f5a17
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-16 01:18:41 +0100

    Edited a variable

--- Hero Born/Assets/Scripts/LearningCurve.cs
+++ Hero Born/Assets/Scripts/LearningCurve.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
-    public int CurrentAge = 30;
+    public int CurrentAge = 18;
 
     // Start is called before the first frame update
     void Start()
