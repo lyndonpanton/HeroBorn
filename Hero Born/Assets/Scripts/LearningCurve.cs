5a071b2cc263023232909ec4356731f18cff8d33
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-16 01:30:24 +0100

    Passed arguments to a method

--- Hero Born/Assets/Scripts/LearningCurve.cs
+++ Hero Born/Assets/Scripts/LearningCurve.cs
@@ -7,10 +7,18 @@ public class LearningCurve : MonoBehaviour
     public int CurrentAge = 18;
     public int AddedAge = 1;
 
+    public const float PI = 3.14f;
+    public string FirstName = "Harrison";
+    public bool isAuthor = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        ComputeAge();
+        int CharacterLevel = 32;
+        int NextSkillLevel = GenerateCharacter("Spike", CharacterLevel);
+
+        Debug.Log(NextSkillLevel);
+        Debug.Log(GenerateCharacter("Faye", CharacterLevel));
     }
 
     // Update is called once per frame
@@ -22,5 +30,12 @@ public class LearningCurve : MonoBehaviour
     void ComputeAge()
     {
         Debug.Log(CurrentAge + AddedAge);
+    }
+
+    int GenerateCharacter(string name, int level)
+    {
+        //Debug.Log($"Character: {name} (lv. {level})");
+
+        return level += 5;
     }
 }
