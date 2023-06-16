23b42faaa6d26714e90bd2ecdd94d47dadbdca60
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-17 00:18:09 +0100

    Player now uses a Rigidbody and Physics

--- Hero Born/Assets/Scripts/PlayerBehaviour.cs
+++ Hero Born/Assets/Scripts/PlayerBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
@@ -10,18 +11,52 @@ public class PlayerBehaviour : MonoBehaviour
     private float _verticalInput;
     private float _horizontalInput;
 
+    private Rigidbody _rb;
+
+    void Start()
+    {
+        if (GetComponent<Rigidbody>() != null)
+        {
+            _rb = GetComponent<Rigidbody>();
+        } else
+        {
+            this.AddComponent<Rigidbody>();
+            _rb = GetComponent<Rigidbody>();
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         _horizontalInput = Input.GetAxis("Horizontal") * RotateSpeed;
         _verticalInput = Input.GetAxis("Vertical") * MoveSpeed;
 
-        this.transform.Translate(
-            _verticalInput * Time.deltaTime * Vector3.forward
+        //this.transform.Translate(
+        //    _verticalInput * Time.deltaTime * Vector3.forward
+        //);
+
+        //this.transform.Rotate(
+        //    _horizontalInput * Time.deltaTime * Vector3.up
+        //);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * _horizontalInput;
+
+        Quaternion angleRotation = Quaternion.Euler(
+            rotation * Time.fixedDeltaTime
         );
 
-        this.transform.Rotate(
-            _horizontalInput * Time.deltaTime * Vector3.up
+        _rb.MovePosition(
+            this.transform.position
+            + (this.transform.forward * _verticalInput * Time.fixedDeltaTime)
+        );
+
+        // Any physics- or Rigidbody-related code always goes inside the FixedUpdate
+        // method, rather than Update or the other MonoBehavior methods
+        _rb.MoveRotation(
+            _rb.rotation * angleRotation
         );
     }
 }
