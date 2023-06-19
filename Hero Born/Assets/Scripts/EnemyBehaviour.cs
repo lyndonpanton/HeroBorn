a0e1e4bc5d63493ed2edb059473f8f94428cb635
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-19 09:14:35 +0100

    Enemy now patrols a defined path

--- Hero Born/Assets/Scripts/EnemyBehaviour.cs
+++ Hero Born/Assets/Scripts/EnemyBehaviour.cs
@@ -1,9 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    public Transform PatrolRoute;
+    public List<Transform> Locations;
+    private int _locationIndex = 0;
+    // UnityEngine.AI gives access to Unity's Navigation classes
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+
+        InitialisePatrolRoute();
+
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if (_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitialisePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+            return;
+
+        _agent.destination = Locations[_locationIndex].position;
+
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
