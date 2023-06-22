e7c963c3f85446b1ab5844b3f3878e7cf8818730
Lyndon Panton <LyndonPanton@gmail.com>
2023-06-22 10:16:25 +0100

    Printed basic filesystem properties

--- Hero Born/Assets/Scripts/DataManager.cs
+++ Hero Born/Assets/Scripts/DataManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
 
+using System.IO;
+using UnityEngine.UIElements;
+
 public class DataManager : MonoBehaviour, IManager
 {
     private string _state;
@@ -28,5 +31,23 @@ public class DataManager : MonoBehaviour, IManager
     {
         _state = "Data Manager initialised";
         Debug.Log(_state);
+
+        FilesystemInfo();
+    }
+
+    public void FilesystemInfo()
+    {
+        // ":" (mac/linux) or ";" (windows/unix)
+        Debug.Log($"Path separator character: {Path.PathSeparator}");
+
+        // "/" (mac/linux) or "\" (windows/unix)
+        Debug.Log($"Directory separator character: {Path.DirectorySeparatorChar}");
+
+        // The full path where HeroBorn directory is stored
+        Debug.Log($"Current directory: {Directory.GetCurrentDirectory()}");
+
+        // The temporary path which is the location of the Filesystem's
+        // temporary folder
+        Debug.Log($"Temporary Path: {Path.GetTempPath()}");
     }
 }
