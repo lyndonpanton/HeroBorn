702f4c59a8aa2c5966f679778f62e8158ff6e010
Lyndon Panton <LyndonPanton@gmail.com>
2023-07-01 13:40:50 +0100

    Created a generic method

--- Hero Born/Assets/Scripts/Shop.cs
+++ Hero Born/Assets/Scripts/Shop.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// T is defined at initialisation and all inner instances of a generic will
+// match the outer most definition
 public class Shop<T>
 {
     //public List<T> inventory = new List<T>();
@@ -10,5 +12,22 @@ public class Shop<T>
     public void AddItem(T newItem)
     {
         inventory.Add(newItem);
+    }
+
+    // A new generic type. Since it is the outer most instance of the generic
+    // called U, it is defined when this method is called
+    public int GetStockCount<U>()
+    {
+        int stock = 0;
+
+        foreach (T item in inventory)
+        {
+            if (item is U)
+            {
+                stock++;
+            }
+        }
+
+        return stock;
     }
 }
